3e0783d8d48f40fab5d226ac3b06a41685fa6033
Ryan Struhl <rwstruhl@gmail.com>
2013-02-06 12:50:24 -0500

    End of day 3

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,6 +1,14 @@
 //Backbone Week Day 3
 
-var Ghost = Backbone.Model.extend({
+window.App = {
+	Models: {},
+	Collections: {},
+	Views: {},
+	Router: {},
+	Helpers: {}
+};
+
+App.Models.Ghost = Backbone.Model.extend({
 	defaults: {
 		name: "Clyde",
 		age: 33,
@@ -19,27 +27,47 @@ var Ghost = Backbone.Model.extend({
 	}
 });
 
-var Ghosts = Backbone.Collection.extend({
-	model: Ghost
+App.Collections.Ghosts = Backbone.Collection.extend({
+	model: App.Models.Ghost,
+
+	url: '/ghosts'
 });
 
-var GhostView = Backbone.View.extend({
+App.Views.Ghost = Backbone.View.extend({
 	tagName: 'li',
 	className: 'ghost',
+
+	initialize: function(){
+		this.model.on('destroy', this.unrender, this);
+	},
+
+	events: {
+		'click .delete': 'destroyMe'
+	},
 
 	template: _.template($('#ghostTemplate').html()),
 
 	render: function(){
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
+	},
+
+	unrender: function(){
+		this.remove();
+	},
+
+	destroyMe: function(e){
+		e.preventDefault();
+		this.model.destroy();
 	}
 });
 
-var GhostsView = Backbone.View.extend({
+App.Views.Ghosts = Backbone.View.extend({
 	tagName: 'ul',
 
 	initialize: function(){
 		this.collection.on('add', this.addOne, this);
+		this.collection.on('reset', this.render, this);
 	},
 
 	render: function(){
@@ -51,25 +79,47 @@ var GhostsView = Backbone.View.extend({
 	},
 
 	addOne: function(ghost){
-		var ghostView = new GhostView({model: ghost});
+		var ghostView = new App.Views.Ghost({model: ghost});
 		this.$el.append(ghostView.render().el);
 	}
 });
 
+App.Views.GhostForm = Backbone.View.extend({
+	template: $('#ghostForm').html(),
+
+	events: {
+		'submit': 'addGhost'
+	},
+
+	render: function(){
+		this.$el.html(this.template);
+
+		return this;
+	},
+
+	addGhost: function(e){
+		e.preventDefault();
+
+		var name = this.$('#name').val(),
+			age = this.$('#age').val(),
+			color = this.$('#color').val(),
+			game = this.$('#game').val();
+
+		this.collection.create({name: name, age: age, color: color, game: game});
+
+		this.$('input[type="text"]').val('');
+	}
+
+});
+
 $(function(){
+	ghosts = new App.Collections.Ghosts();
 
-	ghosts = new Ghosts([
-		{name: "Inky", color: "Blue"}, 
-		{name: "Blinky", color: "Red"}, 
-		{name: "Pinky", color: "Pink"}, 
-		{name: "Clyde", color: "Orange"}, 
-		{name: "Sue", color: "Orange", game: "Ms. Pac-Man", age: 31}
-	]);
+	ghostsView = new App.Views.Ghosts({collection: ghosts});
 
-	ghostsView = new GhostsView({collection: ghosts});
-	ghostFormView = new GhostFormView({collection: ghosts});
+	ghostForm = new App.Views.GhostForm({collection: ghosts});
 
-	$('#ghosts').append(ghostFormView.render().el);
+	$('#ghosts').append(ghostForm.render().el);
 
 	$('#ghosts').append(ghostsView.render().el);
-})
+});
